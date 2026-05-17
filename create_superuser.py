581d0063432fa7da787ccfd8303f6923f1125652
Leman Mirizade <lamanmirizada@gmail.com>
2026-05-17 21:02:06 +0400

    render deploy config

--- create_superuser.py
+++ create_superuser.py
@@ -5,7 +5,6 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolio_backend.settings')
 django.setup()
 
 from django.contrib.auth import get_user_model
-
 User = get_user_model()
 
 username = os.environ.get("DJANGO_SUPERUSER_USERNAME")
@@ -13,14 +12,10 @@ email = os.environ.get("DJANGO_SUPERUSER_EMAIL")
 password = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
 
 user, created = User.objects.get_or_create(username=username)
-
-if created:
-    user.email = email
-    user.is_staff = True
-    user.is_superuser = True
-
-# Hər deployment-da şifrəni yenilə
-user.set_password(password)
+user.email = email
+user.is_staff = True
+user.is_superuser = True
+user.set_password(password)  # həmişə yenilə
 user.save()
 
-print("Superuser created." if created else "Superuser password updated.")
+print(f"{'Created' if created else 'Updated'}: {username}")
