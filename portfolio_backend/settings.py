c9aaf9b16e3e85eeefd32aeb90ea5c5f3b693e51
Leman Mirizade <lamanmirizada@gmail.com>
2026-05-17 21:47:49 +0400

    render deploy config

--- portfolio_backend/settings.py
+++ portfolio_backend/settings.py
@@ -83,7 +83,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
